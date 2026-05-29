ef4f08c15793ae48edc0ef0a2d06fd351a799334
Shiva Kumar Reddy <ramidishivakumarreddy@gmail.com>
2026-05-29 10:11:19 +0530

    'commit'

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://code-judge-p0ne.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
